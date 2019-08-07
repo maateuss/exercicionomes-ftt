6ce2cfdc723290e95f73d37bbfd34fa206831783
Mateus Santos <40566391+maateuss@users.noreply.github.com>
2019-08-07 20:53:49 -0300

    initial commit

---
+++ src/conversaentrepessoas/ConversaEntrePessoas.java
@@ -0,0 +1,15 @@
+package conversaentrepessoas;
+
+import java.util.Scanner;
+
+public class ConversaEntrePessoas {
+    
+    private static Scanner sc = new Scanner(System.in);
+    
+    
+
+    public static void main(String[] args) {
+        System.out.println("Bem vindo, insira o nome da primeira pessoa:");
+        String text = sc.nextLine();
+    }    
+}
---
+++ src/conversaentrepessoas/Pessoa.java
@@ -0,0 +1,27 @@
+
+package conversaentrepessoas;
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+    
+    public String obterNome(){
+        return nome;        
+    }
+    
+    public int obterIdade(){
+        return idade;
+    }
+    
+    public void comprimentar(){
+    }
+    
+    public void questionar(){
+    }
+    
+    public void seApresentar(){
+    }
+    
+    public void seExpressar(){
+    }
+}
